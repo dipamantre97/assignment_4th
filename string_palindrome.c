7c08cae7c321b0d24cb3d90e3b851130dc926b3e
dipamantre97 <87062425+dipamantre97@users.noreply.github.com>
2021-08-11 19:57:04 +0530

    Add files via upload

---
+++ string_palindrome.c
@@ -0,0 +1,36 @@
+#include <string.h>
+ 
+ 
+int checkpalindrome(char s[])
+{
+	int i,j;
+	for(i=0,j=strlen(s)-1;i<=j;i++,j--)
+	if(s[i]!=s[j])
+	{
+		break;
+	}
+	if(i>j)
+	return 1;
+	else
+	return 0;
+    
+ 	 
+ 	
+}
+int main()
+{
+    char s[1000];  
+   
+    printf("Enter  the string: ");
+    gets(s);
+    
+ 
+    if(checkpalindrome(s))
+ 	    printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+}
+
+/*output
+Enter  the string: radar
+string is palindrome*/
